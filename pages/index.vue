2c5969aa9577de922d9312c6e3a59753ac0ba2eb
NawapolNWP <61312970+NawapolNWP@users.noreply.github.com>
2020-02-23 01:59:28 +0700

    Vending_Machine
    Vending Machine

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<<template>
+<div class="my-5">
+    <b-container>
+        <h1 class="head">Vending Machine</h1>
+        <b-row class="my-5">
+            <Beverage/>
+        </b-row>
+    </b-container>
+</div>
+</template>
+
+<script>
+import Beverage from '@/components/Beverage'
+export default {
+    components: {
+      Beverage
+    }
+}
+</script>
+
+<style>
+body{
+  background-color: crimson;
+}
+  .head{
+    text-align: center;
+    font-weight: bold;
+    color: white;
+  }
+</style>
